f85d9d597f93eebef203b486e29a2a290cd9abfd
Philippe Guilbault <pguilbault@funio.com>
2013-06-16 15:46:33 -0400

    Only users in the group admin have access to the web admin

--- includes/post-init.php
+++ includes/post-init.php
@@ -9,3 +9,7 @@ if (in_array($url['path'], array('/mpf-admin/login.php')) || preg_match('@^/mpf-
 
 $user = \MPF\Session::mustBeLoggedIn('/mpf-admin/login.php');
 
+if (!$user->isInGroup(\MPF\User\Group::ADMIN())) {
+    header("Location: /mpf-admin/rest/user/{$user->getUsername()}/logout/?redirect=".urlencode('/mpf-admin/login.php')."\n");
+    exit;
+}
